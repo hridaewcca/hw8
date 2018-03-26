09400416bb6ed58407149a5b5007f3473a114e0b
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-03-25 23:43:03 -0700

    Create doctor.ino

---
+++ doctor.ino
@@ -0,0 +1,20 @@
+//like a heartbeat
+int sensorValue = 0;
+int sensorPin = A0;
+
+void setup() {
+  pinMode(13, OUTPUT);
+
+  Serial.begin(9600);
+}
+void loop() {
+  for (int fadeValue = 0 ; fadeValue <= 1000; fadeValue ++) {
+    analogWrite(13, fadeValue);
+    delay(1);
+  }
+
+  for (int fadeValue = 1000 ; fadeValue >= 0; fadeValue --) {
+    analogWrite(13, fadeValue);
+    delay(1);
+  }
+}
